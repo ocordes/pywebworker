75c6c1fb18e34ca0847f8d347829b17ba2c841e8
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-06-20 14:42:16 +0200

    - work on some CSS bootstrap templates

--- pywebworker/routes.py
+++ pywebworker/routes.py
@@ -14,7 +14,15 @@ from pywebworker.workerapp import socketio, flask_app
 from flask import render_template, session, request, jsonify
 
 
-
+@flask_app.route('/products', methods=['GET'])
+@flask_app.route('/users', methods=['GET'])
+@flask_app.route('/reports', methods=['GET'])
+@flask_app.route('/index', methods=['GET'])
 @flask_app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', debug=True, title='Dashboard')
+
+
+@flask_app.route('/orders', methods=['GET'])
+def base():
+    return render_template('base.html', debug=True, title='Orders')
